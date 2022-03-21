ac3d46305929c4902e6e5d4cfdbbb721ba0bb829
Stéphano Giuseppe Rajão de Paiva <stephano@Stephanos-MacBook-Air.local>
2022-03-20 21:19:18 -0300

    chore: first commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,41 @@
+ //
+//  ContentView.swift
+//  Shared
+//
+//  Created by Stéphano Giuseppe Rajão de Paiva on 20/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }.padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                Text("⚽️").padding()
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Stéphano Giuseppe Rajão de Paiva on 20/03/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
